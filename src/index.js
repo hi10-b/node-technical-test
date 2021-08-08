e511f7352e7076f2e0ac291acef420053085140c
hi10-b <41531756+hi10-b@users.noreply.github.com>
2021-08-08 22:12:08 +1000

    rss feed from nasa

--- src/index.js
+++ src/index.js
@@ -1,11 +1,66 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3002;
+const getRSSFeed = require("./utilities/rss-parser");
+const RSSParser = require("rss-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const parser = new RSSParser();
+
+//create different file for url imput
+const url = "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss";
+
+app.get("/", (req, res) => {
+	try {
+		//rss parser required async
+		(async () => {
+			//xml feed from url
+			const feed = await parser.parseURL(url);
+
+			//create array and view as object
+			let feedArray = [];
+
+			//fetch frist 10
+			// for (var x = 1; x < 10; x++) {
+			// 	feedArray.push(feed.item[x].title);
+			// 	feedArray.push(feed.item[x].content);
+			// }
+			feed.items.forEach((item) => {
+				feedArray.push(item.title);
+				feedArray.push(item.content);
+			});
+			console.log(feed);
+			res.json(feedArray);
+		})();
+
+		// return parser.parseURL(rss_url);
+	} catch (error) {
+		console.log("Error:", error.message);
+	}
+});
+
+//using rss-parser utility
+app.get("/nasa2", (req, res) => {
+	// const feed3 = parser2(url);
+	// var feed = parser.parseURL(url);
+
+	(async () => {
+		const feed4 = await getRSSFeed(url);
+
+		let feedArray4 = [];
+		// feed4.item.forEach((item) => {
+		// 	feedArray4.push(item);
+		// 	console.log("item " + item.title);
+		// });
+		console.log(feed4);
+		res.send(feed4);
+	})();
+
+	// const feedArray = feed3.forEach((items) => {
+	// 	console.log("item " + items.title);
+	// });
+	console.log();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/utilities/rss-parser.js
+++ src/utilities/rss-parser.js
@@ -1,16 +1,45 @@
-const RSSParser = require('rss-parser');
+const RSSParser = require("rss-parser");
 
 const parser = new RSSParser();
 
 // This function uses the rss-parser library to fetch the url that's passed
-// and return a javascript object. 
+// and return a javascript object.
 // For more info see - https://github.com/rbren/rss-parser#readme
+
+// function getRSSFeed(rss_url) {
+// 	try {
+// 		let feed = parser.parseURL(
+// 			"https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss"
+// 		);
+
+// 		console.log("pasered data " + feed);
+// 		return parser.parseURL(rss_url);
+// 	} catch (error) {
+// 		console.log("Error:", error.message);
+// 	}
+// }
+
 function getRSSFeed(rss_url) {
-    try {
-        return parser.parseURL(rss_url);
-    } catch(error) {
-        console.log('Error:', error.message)
-    }
+	try {
+		(async () => {
+			const feed = await parser.parseURL(rss_url);
+
+			let feedArray = [];
+			feed.items.forEach((item) => {
+				feedArray.push(item);
+			});
+			// console.log(feed);
+
+			return feed;
+		})();
+		// return parser.parseURL(rss_url);
+	} catch (error) {
+		console.log("Error:", error.message);
+	}
+
+	// feed.items.forEach((item) => {
+	// 	console.log(item.title + ":" + item.link);
+	// });
 }
 
-module.exports = getRSSFeed
+module.exports = getRSSFeed;
